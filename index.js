5c59b7757e540e08e3dc5258266c1f0869e0e015
Subrata Sarker <tccdcc8@gmail.com>
2022-05-24 08:29:35 +0600

    get all service method added

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+app.get('/', async(req, res)=>{
+    
+})
+
+
 app.get('/', (req, res) => {
     res.send('I am running from server site')
 })
