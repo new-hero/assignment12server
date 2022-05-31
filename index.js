0a185c80e426f893f29994868c4b6fd5a1b85d19
Subrata Sarker <tccdcc8@gmail.com>
2022-05-31 19:13:25 +0600

    application error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
